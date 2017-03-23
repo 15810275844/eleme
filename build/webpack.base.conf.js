587cf542f661916cdc223abe64a554b3e7405e93
unknown <>
2017-03-23 18:44:25 +0800

    修改路径2

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -17,7 +17,7 @@ module.exports = {
     // publicPath: process.env.NODE_ENV === 'production'
     //   ? config.build.assetsPublicPath
     //   : config.dev.assetsPublicPath
-    publicPath:'/'
+    publicPath:'./'
   },
   resolve: {
     extensions: ['.js', '.vue', '.json'], //自动补全后缀 improt
